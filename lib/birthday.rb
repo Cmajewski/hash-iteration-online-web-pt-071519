35ccdaf6447d32ff36a515bbadb8b032cc7f74be
Caitlin Majewski <cmajewski@wesleyan.edu>
2019-08-07 01:47:17 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -1,6 +1,6 @@
 # birthday_kids = {
-#   "Timmy" => 9, 
-#   "Sarah" => 6, 
+#   "Timmy" => 9,
+#   "Sarah" => 6,
 #   "Amanda" => 27
 # }
 
@@ -9,5 +9,3 @@ birthday_kids.each do |kid,age|
   puts "Happy Birthday #{kid}! You are now #{age} years old!"
   # add your code snippet here!
 end
-
-
