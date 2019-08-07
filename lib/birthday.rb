db955b17fe1f732f9a0645fb6a91e776eed52d6f
Caitlin Majewski <cmajewski@wesleyan.edu>
2019-08-07 01:51:42 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -9,3 +9,10 @@ birthday_kids.each do |kid,age|
   puts "Happy Birthday #{kid}! You are now #{age} years old!"
   end
 end
+
+def age_appropriate_birthday(birthday_kids)
+birthday_kids.each do |kid,age|
+  if age<12
+  puts "Happy Birthday #{kid}! You are now #{age} years old!"
+  end
+end
